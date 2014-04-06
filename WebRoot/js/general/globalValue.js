3c2e83b1ddc34216e8537d03d46742159ccd980f
siqicheng <siqicheng.fdu@gmail.com>
2014-04-06 16:33:58 +0800

    add login cookie

---
+++ WebRoot/js/general/globalValue.js
@@ -0,0 +1,11 @@
+/*
+ * # Truthbook Global Values JavaScript
+ *
+ */
+
+ServerRoot = "http://localhost:8080/truthbook/services/";
+ServiceType = {
+            	   LOGIN:"loginService/",
+            	   USERPROFILE :"userProfile/"
+               };
+HomePage = "http://localhost:8080/truthbook/profile_test.html";
