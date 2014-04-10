0ecf12d1a0ae46a2e530566a43258036a8b180fb
dsdshcym <dsdshcym@gmail.com>
2014-04-10 19:50:56 +0800

    small ui adjustments

--- WebRoot/js/general/utility.js
+++ WebRoot/js/general/utility.js
@@ -143,7 +143,6 @@ function showPopup(){
 	$("#upload").show();
 }
 
-//TODO: 返回好友关系
 function getRelationship(friendId) {
 	for(friend in friendsId.eFriends) {
 		if(friendId == friendsId.eFriends[friend]["userId"]) {
