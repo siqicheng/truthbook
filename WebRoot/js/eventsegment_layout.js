6ad88bf7007ddd4faf7f1e08d4d769d1fe31a227
wuzeyou <wuzeyouttsz@gmail.com>
2014-04-17 19:31:08 +0800

    modified the CSS of profile page & modified the itemLayout in eventsegment

--- WebRoot/js/eventsegment_layout.js
+++ WebRoot/js/eventsegment_layout.js
@@ -351,51 +351,47 @@ var items = [
              ];
 
 $(document).ready(function() {
-	//layout items 
-	//items = [];
 	itemLayout(items);
-	//initialize all items
-//	itemInitialize();
-	
-	itemLayout(items);
-	//initialize all items
-	itemInitialize();
-	
+	$('.testitemload').click(function(){
+		$('#eventsegment').masonry('destroy');
+		itemLayout(items);
+	});
 });
 
+function itemLayout(items){
+	var newitems = itemCreate(items);
+	$('#eventsegment').append(newitems).masonry({		
+		itemSelector: '.eventpile',
+		gutter: 11});
+	itemInitialize();
+}
 
 
 function itemInitialize(){
-	//masonry initialization
-	var $container=$("#eventsegment").masonry({		
-			itemSelector: '.eventpile',
-			gutter: 11
-		});
-		
 		//shape flip
 	$(".commentToggle").click(function(){		
 		$(this).parents('.ui.shape').shape('flip over');
-		$container.masonry();		
+		$('#eventsegment').masonry();		
 	});
 		
 		//After all images have loaded, rerange the masonry item
-	$container.imagesLoaded( function() {
-		$container.masonry();
+	$('#eventsegment').imagesLoaded( function() {
+		$('#eventsegment').masonry();
 	});
 		
 		//force the mansonry layout run
 	$("#eventsegment").mouseover(function(){
-		$container.masonry();
+		$('#eventsegment').masonry();
 	});
 		
 		
 	//buttons on images show on/off
 	$("#eventsegment .eventpile .item .image").hover(function(){
-		    $(this).children("a").fadeIn("fast");
+		    $(this).children(".imgbtnArea").fadeIn("fast");
 		    $(this).children("img").fadeTo("fast",0.9);
 				},
 				function(){
-		    $(this).children("a").fadeOut("fast");
+		    $(this).children(".imgbtnArea").fadeOut("fast");
 		    $(this).children("img").fadeTo("fast",1);
 			}
 		);
@@ -422,54 +418,70 @@ function eventRemove($removeBtn) {
 	$("#eventsegment").masonry( 'remove', rmelement);
 }
 
-function itemLayout(items) {
+function itemCreate(items) {
+	var elems=[];
 	for(var i=0, len=items.length;i< len;i++){
 		var item = items[i];
 		var comment_num = item.comments.length;
 		var comment='';
-		for(var j=0;j< comment_num;j++){
-			var comment_item = item.comments[j];
-			comment += ("\t\t<div class='comment'>\n" +
-							"\t\t\t<a class='avatar' href='"+comment_item.url+"'>" +
-									"<img src='"+comment_item.avatar+"'></a>\n"+
-							"\t\t\t<div class='content'>\n"+
-							"\t\t\t\t<a class='author' href='"+comment_item.url+"'>"+comment_item.username+"</a>\n"+
-							"\t\t\t\t<div class='metadata'><span class='date'>"+comment_item.meta+"</span></div>\n"+
-							"\t\t\t\t<div class='text'>"+comment_item.content+"</div>\n"+
-							"\t\t\t</div>\n"+
-						"\t\t</div>\n");
+		if (comment_num > 0) {
+			for(var j=0;j< comment_num;j++){
+				var comment_item = item.comments[j];
+				comment += ("\t\t<div class='comment'>\n" +
+								"\t\t\t<a class='avatar' href='"+comment_item.url+"'>" +
+										"<img src='"+comment_item.avatar+"'></a>\n"+
+								"\t\t\t<div class='content'>\n"+
+								"\t\t\t\t<a class='author' href='"+comment_item.url+"'>"+comment_item.username+"</a>\n"+
+								"\t\t\t\t<div class='metadata'><span class='date'>"+comment_item.meta+"</span></div>\n"+
+								"\t\t\t\t<div class='text'>"+comment_item.content+"</div>\n"+
+								"\t\t\t</div>\n"+
+							"\t\t</div>\n");
+			}
 		}
-		$("#eventsegment").append(
+		var newItem =
+//		$("#eventsegment").append(
 				"<div class='eventpile'>\n" +
                     "<div class='ui shape'>\n" +
                     "\t<div class='sides'>\n" +
-                    "\t\t<div class='active side ui items'>" +
-                    "\t\t\t<div class='item'>" +
+                    "\t\t<div class='active side ui items'>\n" +
+                    "\t\t\t<div class='item'>\n" +
 					"\t\t\t<div class='image'>\n"+
 						"\t\t\t\t<img src='"+item.imgsrc+"'>\n"+
-						"\t\t\t\t<a class='ui tiny circular button likebtn'>&ensp;<i class='heart tiny icon'></i></a>\n"+
+						"\t\t\t\t<div class='imgbtnArea'>\n" +
+						"\t\t\t\t\t<div class='ui tiny button likebtn'><i class='heart tiny icon'></i></div>\n"+
+						"\t\t\t\t\t<div class='ui tiny red inverted button commentbtn commentToggle'>\n" +
+						"\t\t\t\t\t\t<i class='comment icon'></i>"+comment_num+"</div>\n" +
+						"\t\t\t\t</div>\n"+
 					"\t\t\t</div>\n"+
 					"\t\t\t<div class = 'content'>\n"+
 						"\t\t\t\t<p class='description'>"+item.descript+"</p>\n"+
-						"\t\t\t\t<div class='meta'>"+"uploaded by <a class='uploader' href='"+item.uploader.url+"'>"+
+						"\t\t\t\t<div class='meta'>"+"by <a class='uploader' href='"+item.uploader.url+"'>"+
 							item.uploader.username+"</a> "+
 							item.meta+"</div>\n"+
 					"\t\t\t</div>\n"+
 					"\t\t\t<div class='btnArea'>\n" +
-                    "\t\t\t<button class='ui tiny red inverted animated button commentToggle'>\n" +
-                    "\t\t\t\t<div class='visible content'>&ensp;<i class='comment icon'></i>&ensp;</div>\n" +
-                    "\t\t\t\t<div class='hidden content'>评论(2)</div>\n" +
-                    "\t\t\t</button>\n" +
-                    "\t\t\t<button class='ui tiny basic animated button eventRemove'>\n" +
-                    "\t\t\t\t<div class='visible content'>&ensp;<i class='remove icon'></i>&ensp;</div>\n" +
-                    "\t\t\t\t<div class='hidden content'>删除</div>\n" +
-                    "\t\t\t</button>\n" +
+                    "\t\t\t<div class='ui tiny basic animated button rePost'>\n" +
+                    "\t\t\t\t<div class='visible content'><i class='reply mail icon'></i></div>\n" +
+                    "\t\t\t\t<div class='hidden content'>返还照片</div>\n" +
+                    "\t\t\t</div>\n" +
+                    "\t\t\t<div class='ui tiny basic animated button uploadFor'>\n" +
+                    "\t\t\t\t<div class='visible content'><i class='cloud upload icon'></i></div>\n" +
+                    "\t\t\t\t<div class='hidden content'>为其上传</div>\n" +
+                    "\t\t\t</div>\n" +
+                    "\t\t\t<div class='ui tiny basic animated button setPortrait'>\n" +
+                    "\t\t\t\t<div class='visible content'><i class='smile icon'></i></div>\n" +
+                    "\t\t\t\t<div class='hidden content'>设为头像</div>\n" +
+                    "\t\t\t</div>\n" +
+                    "\t\t\t<div class='ui tiny basic animated button eventRemove'>\n" +
+                    "\t\t\t\t<div class='visible content'><i class='remove icon'></i></div>\n" +
+                    "\t\t\t\t<div class='hidden content'>删除照片</div>\n" +
+                    "\t\t\t</div>\n" +
                     "\t\t\t</div>\n" +
                     "\t\t\t</div>\n" +
                     "\t\t</div>\n" +
-					"\t\t<div class='side ui items'>" +
-                    "\t\t\t<div class='item'>" +
-                    "\t\t\t<h2 class='ui header'>所有评论</h2>" +
+					"\t\t<div class='side ui items'>\n" +
+                    "\t\t\t<div class='item'>\n" +
+                    "\t\t\t<h2 class='ui header'>所有评论</h2>\n" +
 					"\t\t\t<div class='ui minimal comments'>\n"+comment+
 					"\t\t\t\t<form class='ui reply form'>\n"+
 					"\t\t\t\t\t<div class='field'><textarea placeholder='你想说…'></textarea></div>\n"+
@@ -486,7 +498,9 @@ function itemLayout(items) {
                     "\t\t</div>\n" +
                     "\t</div>\n" +
                     "</div>\n" +
-				"</div>\n");
+				"</div>\n";
+		elems.push( newItem );
 	}
+	return elems;
 }
 
