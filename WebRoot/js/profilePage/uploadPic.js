14d7e69e7e60abd29bfc13b91b34a5871c26dd19
dsdshcym <dsdshcym@gmail.com>
2014-04-29 12:33:08 +0800

    upload image preview adjusts

--- WebRoot/js/profilePage/uploadPic.js
+++ WebRoot/js/profilePage/uploadPic.js
@@ -420,36 +420,6 @@ function resetUpload() {
 	$("#entryTime").removeAttr("disabled");
 }
 
-//获取本地图片路径，并显示在预览框中
-function readURL(input) {
-    if (input.files && input.files[0]) {
-    	$('#img_prev').show();
-        var reader = new FileReader();
-        reader.onloadend = function (e) {
-        	$("#imgPrev").attr("src", e.target.result); 
-        };
-        reader.readAsDataURL(input.files[0]);
-    } else {
-        //IE情况
-        var docObj = document.getElementByIdx_x('fileElem');
-        docObj.select();
-        //解决IE9下document.selection拒绝访问的错误
-        docObj.blur();
-        var imgSrc = document.selection.createRange().text;
-        var img_prevId = document.getElementByIdx_x("img_prev");
-        $('#img_prev').width(150).height(200); //必须设置初始大小
-        //图片异常的捕捉，防止用户修改后缀来伪造图片
-        try {
-            img_prevId.style.filter = "progid:DXImageTransform.Microsoft.AlphaImageLoader(sizingMethod=scale)";
-            img_prevId.filters.item("DXImageTransform.Microsoft.AlphaImageLoader").src = imgSrc;
-        } catch (e) {
-            alert("您上传的图片格式不正确，请重新选择!"); return false;
-        }
-        $('#img_prev').hide();
-        document.selection.empty();
-    }
-};
-
 /*step之间的跳转*/
 function gotoChoosePeople() {
 	if(! upload_for_friend) {picReceiver = null;};
